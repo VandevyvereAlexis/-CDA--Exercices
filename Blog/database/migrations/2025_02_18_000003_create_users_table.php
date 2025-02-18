6cdaec0d3edb38fb317ef49af345b152cbae7782
VandevyvereAlexis <131187655+VandevyvereAlexis@users.noreply.github.com>
2025-02-19 00:21:06 +0100

    [BLOG] Mise en place et configuration des migrations

--- Blog/database/migrations/2025_02_18_000003_create_users_table.php
+++ Blog/database/migrations/2025_02_18_000003_create_users_table.php
@@ -13,7 +13,8 @@ return new class extends Migration
     {
         Schema::create('users', function (Blueprint $table) {
             $table->id();
-            $table->string('name');
+            $table->foreignId('role_id')->default(1)->constrained('roles')->onDelete('restrict');
+            $table->string('pseudo', 100)->unique();
             $table->string('email')->unique();
             $table->timestamp('email_verified_at')->nullable();
             $table->string('password');
